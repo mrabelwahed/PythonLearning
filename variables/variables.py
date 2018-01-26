a3998e807e864edd57c50be3242f54b2cd04299a
elsamet <eng.mahmoudramadan2012@gmail.com>
2018-01-26 15:45:31 +0200

    add variables file

---
+++ variables/variables.py
@@ -0,0 +1,15 @@
+# int numbers 
+num = 90
+print(num, type(num))
+num = float(num)
+print(num)
+
+#string 
+str = "my name is reem"
+print(str, type(str))
+
+data = "{} is the amount".format(num)
+print(data,type(data))
+print(len(data))
+print(data[0])
+
